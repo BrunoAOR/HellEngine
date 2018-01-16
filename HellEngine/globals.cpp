8582117a8ba2c27cb05c766a3cfce8966b779c4f
Bruno Ortiz <bruno.ortiz.r@gmail.com>
2018-01-16 17:48:39 +0100

    Include helper function to load json files
    - Include a LoadJson function to the globals file

--- HellEngine/globals.cpp
+++ HellEngine/globals.cpp
@@ -1,8 +1,11 @@
+#include <assert.h>
+#include <fstream>
 #include <stdio.h>
 #include <windows.h>
+#include "Json/json.h"
 #include "globals.h"
 
-void log(const char file[], int line, const char* format, ...)
+void Log(const char file[], int line, const char* format, ...)
 {
 	static char tmp_string[4096];
 	static char tmp_string2[4096];
@@ -14,4 +17,13 @@ void log(const char file[], int line, const char* format, ...)
 	va_end(ap);
 	sprintf_s(tmp_string2, 4096, "%s(%d) : %s\n", file, line, tmp_string);
 	OutputDebugString(tmp_string2);
+}
+
+Json LoadJson(const char* path)
+{
+	std::ifstream file(path);
+	assert(file.good());
+	Json json;
+	file >> json;
+	return json;
 }
