c951cda9b27c5da717cab39513ba2a00904bd37d
Marc Campins <marcampinscoll@gmail.com>
2018-02-07 13:29:25 +0100

    Refactor ModuleEditorCamera to only have the mouse and keyboard input management functions.
    - Add a ComponentCamera pointer variable that will hold all the actual camera information.
    - Every function in the ModuleEditorCamera that modifies the parameters of the editor camera will do so through this pointer.

--- HellEngine/ModuleEditorCamera.h
+++ HellEngine/ModuleEditorCamera.h
@@ -3,6 +3,7 @@
 
 #include "MathGeoLib/src/Geometry/Frustum.h"
 #include "Color.h"
+#include "ComponentCamera.h"
 #include "Module.h"
 
 class ModuleEditorCamera :
@@ -14,40 +15,9 @@ public:
 
 	bool Init();
 	UpdateStatus Update();
-
-	const float* getPosition();
-
-	/* Sets the position of the camera */
-	void SetPosition(float x, float y, float z);
-
-	/* Sets the vertical FOV of the camera and adjust the horizontal FOV accordingly */
-	bool SetFOV(float fov);
-
-	float getHorizontalFOV() const;
-
-	float getVerticalFOV() const;
-
-	float getAspectRatio() const;
-
-	/* Returns a float* to the first of 16 floats representing the view matrix */
-	float* GetViewMatrix();
-	
-	/* Returns a float* to the first of 16 floats representing the projection matrix */
-	float* GetProjectionMatrix();
-
-	/* Sets the distance for the near and far clipping planes */
-	bool SetPlaneDistances(float near, float far);
-
-	float getNearPlaneDistance() const;
-
-	bool SetNearPlaneDistance(float near);
-
-	float getFarPlaneDistance() const;
-
-	bool SetFarPlaneDistance(float far);
 	
 	/* Method to be called when the window is resized */
-	void onWindowResize();
+	void OnWindowResize();
 
 	/* Method to be called when yaw rotation is required */
 	void RotateYaw(const int direction);
@@ -59,17 +29,8 @@ public:
 
 	void DragCameraVerticalAxis(int direction, vec &frustumPos, float speed);
 
-	const float* GetFront() const;
-
-	void SetFront(float x, float y, float z);
-
-	const float* GetUp() const;
-
-	void SetUp(float x, float y, float z);
-
 public:
-
-	Color background;
+	ComponentCamera* camera;
 	float moveSpeed;
 	float rotationSpeed;
 	float zoomSpeed;
@@ -79,19 +40,8 @@ public:
 
 private:
 
-	void handleCameraMotion();
-	void handleCameraRotation();
-	float getHorizontalFOVrad() const;
-
-private:
-
-	Frustum frustum;
-	float4x4 viewMatrix;
-	float4x4 projectionMatrix;
-	float aspectRatio;
-	float verticalFOVRad;
-	float nearClippingPlane;
-	float farClippingPlane;
+	void HandleCameraMotion();
+	void HandleCameraRotation();
 };
 
 #endif // !__H_MODULE_EDITOR_CAMERA__
