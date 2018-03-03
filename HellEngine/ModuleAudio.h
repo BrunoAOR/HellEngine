2ae15f4482e19fa3934cc72c42bb77f2d32b82bf
Mac092 <m.rodmur@gmail.com>
2018-03-03 19:54:27 +0100

    Prepare ModuleAudio for Bass: - Add Bass project directory to our project properties - Clean ModuleAudio previous implementation

--- HellEngine/ModuleAudio.h
+++ HellEngine/ModuleAudio.h
@@ -4,12 +4,6 @@
 #include <vector>
 #include "Module.h"
 
-#define DEFAULT_MUSIC_FADE_TIME 2.0f
-
-struct _Mix_Music;
-struct Mix_Chunk;
-typedef struct _Mix_Music Mix_Music;
-
 class ModuleAudio : public Module
 {
 public:
@@ -20,19 +14,9 @@ public:
 	bool Init();
 	bool CleanUp();
 
-	/* Play a music file */
-	bool PlayMusic(const char* path, float fadeTime = DEFAULT_MUSIC_FADE_TIME);
-
-	/* Load a WAV in memory */
-	unsigned int LoadFx(const char* path);
-
-	/* Play a previously loaded WAV */
-	bool PlayFx(unsigned int fx, int repeat = 0);
-
 private:
 
-	Mix_Music*	music = nullptr;
-	std::vector<Mix_Chunk*>	fx;
+	
 };
 
 #endif /* __H_MODULEAUDIO__ */
