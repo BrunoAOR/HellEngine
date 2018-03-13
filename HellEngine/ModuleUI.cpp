0c2f4f9fa72393297d0c7b7ff58840a5320b234a
Dani Reyes <dani-kingdom@hotmail.com>
2018-03-13 13:55:28 +0100

    Add Update base system for all UI components

--- HellEngine/ModuleUI.cpp
+++ HellEngine/ModuleUI.cpp
@@ -1,9 +1,4 @@
 #include "ModuleUI.h"
-#include "ComponentUIElement.h"
-#include "ComponentUiButton.h"
-#include "ComponentUiImage.h"
-#include "ComponentUiLabel.h"
-#include "ComponentUiInputText.h"
 #include "GameObject.h"
 
 #include "Glew/include/glew.h"
@@ -24,8 +19,62 @@ UpdateStatus ModuleUI::Update()
     {
         //Check UI states ("pressed, etc...")
         glDisable(GL_DEPTH_TEST);
-
+        UpdateElements();
         glEnable(GL_DEPTH_TEST);
     }
     return UpdateStatus::UPDATE_CONTINUE;
+}
+
+GameObject* ModuleUI::GetClickedGameObject()
+{
+    return clicked;
+}
+GameObject* ModuleUI::GetHoveringGameObject()
+{
+    return hovering;
+}
+
+void ModuleUI::UpdateElements()
+{
+    if (!canvas->GetActive()) return;
+
+}
+
+void ModuleUI::UpdateComponent(Component* component) 
+{
+    switch ((UIElementType)component->GetType())
+    {
+    case UIElementType::IMG:
+        UpdateImage((ComponentUiImage*) component);
+        break;
+    case UIElementType::BUTTON:
+        UpdateButton((ComponentUiButton*)component);
+        break;
+    case UIElementType::LABEL:
+        UpdateLabel((ComponentUiLabel*)component);
+        break;
+    case UIElementType::INPUT_TEXT:
+        UpdateInputText((ComponentUiInputText*)component);
+        break;
+    }
+}
+
+void ModuleUI::UpdateImage(ComponentUiImage* image)
+{
+
+}
+
+void ModuleUI::UpdateButton(ComponentUiButton* button)
+{
+
+}
+
+void ModuleUI::UpdateLabel(ComponentUiLabel* label)
+{
+
+}
+
+void ModuleUI::UpdateInputText(ComponentUiInputText* inputText)
+{
+
 }
--- HellEngine/ModuleUI.h
+++ HellEngine/ModuleUI.h
@@ -2,12 +2,12 @@
 #define __H_MODULE_UI__
 
 #include "Module.h"
+#include "ComponentUIElement.h"
+#include "ComponentUiButton.h"
+#include "ComponentUiImage.h"
+#include "ComponentUiLabel.h"
+#include "ComponentUiInputText.h"
 
-class ComponentUIElement;
-class ComponentUiButton;
-class ComponentUiImage;
-class ComponentUiLabel;
-class ComponentUiInputText;
 class GameObject;
 
 class ModuleUI : public Module
@@ -18,21 +18,35 @@ public:
 
     UpdateStatus Update();
 
-	//UIElement Factory
-	//static ComponentUIElement * NewUIElement(const UIElementType &uiType, GameObject * goOwner)
-	//{
-	//	if (uiType == UIElementType::IMG)
-	//		return new ComponentUiImage(goOwner);
-	//	if (uiType == UIElementType::BUTTON)
-	//		return new ComponentUiButton(goOwner);
-	//	if (uiType == UIElementType::LABEL)
-	//		return new ComponentUiLabel(goOwner);
-	//	if (uiType == UIElementType::INPUT_TEXT)
-	//		return new ComponentUiInputText(goOwner);
-	//	return nullptr;
-	//}
+    //UIElement Factory
+	static ComponentUIElement * NewUIElement(const UIElementType &uiType, GameObject * goOwner)
+	{
+		if (uiType == UIElementType::IMG)
+			return new ComponentUiImage(goOwner);
+		if (uiType == UIElementType::BUTTON)
+			return new ComponentUiButton(goOwner);
+		if (uiType == UIElementType::LABEL)
+			return new ComponentUiLabel(goOwner);
+		if (uiType == UIElementType::INPUT_TEXT)
+			return new ComponentUiInputText(goOwner);
+		return nullptr;
+	}
+
+    GameObject* GetClickedGameObject();
+    GameObject* GetHoveringGameObject();
+
+private:
+    void UpdateElements();
+    void UpdateComponent(Component* component);
+    void UpdateImage(ComponentUiImage* image);
+    void UpdateButton(ComponentUiButton* button);
+    void UpdateLabel(ComponentUiLabel* label);
+    void UpdateInputText(ComponentUiInputText* inputText);
+
 private:
     GameObject* canvas = nullptr;
+    GameObject* clicked = nullptr;
+    GameObject* hovering = nullptr;
 };
 #endif __H_MODULE_UI__
 
