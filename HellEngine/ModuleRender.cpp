278f81c87c1271af0a7a25976ed47c0aa6164ca6
Marc Campins Coll <marcampinscoll@gmail.com>
2018-03-15 20:40:13 +0100

    Disable backface culling as we add transparency and alpha channel

--- HellEngine/ModuleRender.cpp
+++ HellEngine/ModuleRender.cpp
@@ -151,7 +151,7 @@ bool ModuleRender::InitOpenGL() const
 	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 	glEnable(GL_BLEND);
 	glEnable(GL_DEPTH_TEST);
-	glEnable(GL_CULL_FACE);
+	//glEnable(GL_CULL_FACE);
 	glDisable(GL_LIGHTING);
 	glEnable(GL_COLOR_MATERIAL);
 	glEnable(GL_TEXTURE_2D);
